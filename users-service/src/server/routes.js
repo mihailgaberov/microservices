263647e815b90a8c4f64a2e05c475025275aaf2f
mihailgaberov <mihail.gaberov@gmail.com>
2020-01-24 08:13:54 +0200

    :sparkles: Added bcryptjs for hashing passwords.

--- users-service/src/server/routes.js
+++ users-service/src/server/routes.js
@@ -1,21 +1,24 @@
 import { User } from "#root/db/models";
 import generateUUID from "#root/helpers/generateUUID";
+import hashPassword from "#root/helpers/hashPassword";
 
 const setupRoutes = app => {
-  app.post("/users", async  (req, res, next) => {
-      if (!req.body.email || !req.body.password) {
-        return next(new Error("Invalid body."));
-      }
+  app.post("/users", async (req, res, next) => {
+    if (!req.body.email || !req.body.password) {
+      return next(new Error("Invalid body."));
+    }
 
-      try {
-        const newUser = await User.create({
-          email: req.body.email,
-          id: generateUUID(),
-          passwordHash: hashPassword(req.body.password)
-        });
-      } catch (e) {
+    try {
+      const newUser = await User.create({
+        email: req.body.email,
+        id: generateUUID(),
+        passwordHash: hashPassword(req.body.password)
+      });
 
-      }
+      return res.json(newUser);
+    } catch (e) {
+      return next(e);
+    }
   });
 };
 
