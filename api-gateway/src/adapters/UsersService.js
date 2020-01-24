917ecc4342fe2ac3492301ae2ad139544f8b6486
mihailgaberov <mihail.gaberov@gmail.com>
2020-01-24 18:41:08 +0200

    :sparkles: Added createUser resolver.

---
+++ api-gateway/src/adapters/UsersService.js
@@ -0,0 +1,13 @@
+import got from "got";
+
+const USERS_SERVICE_URI = "http://users-service:7101";
+
+export default class UsersService {
+  static async createUser({email, password}) {
+    const body = await got.post(`${USERS_SERVICE_URI}/users`, {
+      json: { email, password }
+    }).json();
+
+    return body;
+  }
+}
--- api-gateway/src/graphql/resolvers/Mutation/createUser.js
+++ api-gateway/src/graphql/resolvers/Mutation/createUser.js
@@ -1,5 +1,7 @@
-const createUserResolver = () => {
-  //
+import UsersService from "#root/adapters/UsersService";
+
+const createUserResolver = async (obj, { email, password }) => {
+  return UsersService.createUser({ email, password });
 };
 
 export default createUserResolver;
