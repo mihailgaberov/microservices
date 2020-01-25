43383d90f46837d7d918e6c11c4e65e0f35c6bfa
mihailgaberov <mihail.gaberov@gmail.com>
2020-01-25 12:53:50 +0200

    :sparkles: Added a route for user sessions (login).

--- users-service/src/server/routes.js
+++ users-service/src/server/routes.js
@@ -1,8 +1,44 @@
+import { addHours } from "date-fns";
 import { User } from "#root/db/models";
 import generateUUID from "#root/helpers/generateUUID";
 import hashPassword from "#root/helpers/hashPassword";
+import passwordCompareSync from "#root/helpers/passwordCompareSync";
+import { UserSession } from "../db/models";
+
+const USER_SESSION_EXPIRY_HOURS = 1;
 
 const setupRoutes = app => {
+  app.post("/sessions", async (req, res, next) => {
+    if (!req.body.email || !req.body.password) {
+      return next(new Error("Invalid body."));
+    }
+
+    try {
+      const user = User.findOne({ attributes: {}, where: { email: req.body.email} });
+
+      if (!user) {
+        return next(new Error("Invalid email."));
+      }
+
+      if (!passwordCompareSync(req.body.password, user.passwordHash)) {
+        return next(new Error("Invalid password."));
+      }
+
+      const expiresAt = addHours(new Date(), USER_SESSION_EXPIRY_HOURS);
+      const sessionToken = generateUUID();
+      const userSession = UserSession.create({
+        expiresAt,
+        id: sessionToken,
+        userId: user.id
+      });
+
+      return res.json(userSession);
+
+    } catch (e) {
+
+    }
+  });
+
   app.post("/users", async (req, res, next) => {
     if (!req.body.email || !req.body.password) {
       return next(new Error("Invalid body."));
