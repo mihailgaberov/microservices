216d281f3dd64eb8e6f2b22f5a1f7cb41d79771a
mihailgaberov <mihail.gaberov@gmail.com>
2020-01-25 12:19:17 +0200

    :sparkles: Added main front end structure of Classifieds app.

--- classifieds-app/src/components/Root/Root.js
+++ classifieds-app/src/components/Root/Root.js
@@ -1,6 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  margin: 0 auto;
+  width: 80rem;
+`;
+
+const Content = styled.div`
+  flex: 1;
+  margin-right: 1rem; 
+  background-color: blue;
+`;
+
+const Sidebar = styled.div`
+background-color: red;
+  flex: 0 auto;
+  width: 10rem;
+`;
+
 const Wrapper = styled.div`
   box-sizing: border-box;
   height: 100%;
@@ -9,7 +28,12 @@ const Wrapper = styled.div`
 `;
 
 const Root = () => {
-  return <Wrapper>hi</Wrapper>
+  return <Wrapper>
+    <Container>
+      <Content>content</Content>
+      <Sidebar>Sidebar</Sidebar>
+    </Container>
+  </Wrapper>
 };
 
 export default Root;
