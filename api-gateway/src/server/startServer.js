4c68a7e07e597bd0063082d05783081e188abc65
mihailgaberov <mihail.gaberov@gmail.com>
2020-01-25 10:49:39 +0200

    :racehorse: Added formatting of the graphql errors.

--- api-gateway/src/server/startServer.js
+++ api-gateway/src/server/startServer.js
@@ -6,10 +6,12 @@ import express from "express";
 import resolvers from "#root/graphql/resolvers";
 import typeDefs from "#root/graphql/typeDefs";
 import accessEnv from "#root/helpers/accessEnv";
+import formatGraphQLErrors from "./formatGraphQLErrors";
 
 const PORT = accessEnv("PORT", 7000);
 
 const apolloServer = new ApolloServer({
+  formatError: formatGraphQLErrors,
   resolvers,
   typeDefs
 });
