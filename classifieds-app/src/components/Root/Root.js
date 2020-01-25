39786dcf9d19ef58176f5b05e337a25cde36fefb
mihailgaberov <mihail.gaberov@gmail.com>
2020-01-25 11:49:18 +0200

    :sparkles: Added styled-components. Styling Root component in Classifieds App.

---
+++ classifieds-app/src/components/Root/Root.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  padding: 1rem;
+`;
+
+const Root = () => {
+  return <Wrapper>hi</Wrapper>
+};
+
+export default Root;
--- classifieds-app/src/index.js
+++ classifieds-app/src/index.js
@@ -1,4 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
+import Root from "#root/components/Root";
+import { createGlobalStyle } from "styled-components";
 
-render(<h1>Working</h1>, document.getElementById("app"));
+const GlobalStyle = createGlobalStyle`
+  @import url("https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap");
+  
+  html, body, #app {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  
+  body {
+    font-family: Roboto, sans-serif;
+  }
+`;
+
+render(<>
+  <GlobalStyle />
+  <Root />
+  </>, document.getElementById("app"));
