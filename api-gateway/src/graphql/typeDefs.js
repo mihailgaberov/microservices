d9f1e9ce6009bbe0f427bc25e1f9d350bc83b2b4
mihailgaberov <mihail.gaberov@gmail.com>
2020-01-25 20:02:41 +0200

    :sparkles: Adding UserSession mutation.

--- api-gateway/src/graphql/typeDefs.js
+++ api-gateway/src/graphql/typeDefs.js
@@ -12,8 +12,16 @@ const typeDefs = gql`
     id: ID!
   }
   
+  type UserSession {
+    createdAt: Date!,
+    expiresAt: Date!,
+    id: ID!,
+    user: User!
+  }
+  
   type Mutation {
     createUser(email: String!, password: String!): User!
+    createUserSession(email: String!, password: String!): UserSession!
   }
   
   type Query {
