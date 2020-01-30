030aeb3d417109a86ecc1a173454bd522c67916a
mihailgaberov <mihail.gaberov@gmail.com>
2020-01-30 08:12:40 +0200

    `:construction: Added injectSession middleware.

---
+++ api-gateway/src/server/injectSession.js
@@ -0,0 +1,14 @@
+import UsersService from "#root/adapters/UsersService";
+
+const injectSession = async (req, res, next) => {
+  if (req.cookies.userSessionId) {
+    const userSession = await UsersService.fetchUserSession({
+      sessionId: req.cookies.userSessionId
+    });
+    res.locals.userSession = userSession;
+  }
+
+  return next();
+};
+
+export default injectSession;
