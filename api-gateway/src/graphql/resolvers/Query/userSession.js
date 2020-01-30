b7e82e180203d38394f76dc683c7b590402eda48
mihailgaberov <mihail.gaberov@gmail.com>
2020-01-30 08:32:30 +0200

    :bug: Fixed wrong if statement.

--- api-gateway/src/graphql/resolvers/Query/userSession.js
+++ api-gateway/src/graphql/resolvers/Query/userSession.js
@@ -1,5 +1,5 @@
 const userSessionResolver = async (obj, args, context) => {
-  if (!args.me !== true) throw new Error("Unsupported argument value");
+  if (args.me !== true) throw new Error("Unsupported argument value");
 
   return context.res.locals.userSession;
 };
