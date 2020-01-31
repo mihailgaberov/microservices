3353bcb483a56cee97ca372f24fb4f518ca739f7
mihailgaberov <mihail.gaberov@gmail.com>
2020-01-31 08:02:57 +0200

    :construction: Adding actions/creators.

---
+++ classifieds-app/src/store/ducks/index.js
@@ -0,0 +1,23 @@
+// actions
+const CLEAR = "session/CLEAR";
+const SET = "session/SET";
+
+const DEFAULT_STATE = null;
+
+// reducer
+const sessionReducer = (state = DEFAULT_STATE, action = {}) => {
+  switch (action.type) {
+    case SET:
+      return action.session;
+    case CLEAR:
+      return null;
+  }
+  return state;
+};
+
+export default sessionReducer;
+
+// action creators
+export const setSession = session => {
+  return { session, type: SET };
+};
