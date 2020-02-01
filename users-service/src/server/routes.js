1c1bdca9157a9c16f11939c80493b1d0f550278b
mihailgaberov <mihail.gaberov@gmail.com>
2020-02-01 10:13:38 +0200

    :sparkles: [Part 8] Added route for deleting user session.

--- users-service/src/server/routes.js
+++ users-service/src/server/routes.js
@@ -40,6 +40,21 @@ const setupRoutes = app => {
     }
   });
 
+  app.delete("sessions/:sessionId", async (req, res, next) => {
+    try {
+      const userSession = await UserSession.findByPk(req.params.sessionId);
+
+      if (!userSession) return next(new Error("Invalid session ID"));
+
+      await userSession.destroy();
+
+      return res.end();
+    }
+    catch (e) {
+      return next(e);
+    }
+  });
+
   app.get("/sessions/:sessionId", async (req, res, next) => {
     try {
       const userSession = await UserSession.findByPk(req.params.sessionId);
