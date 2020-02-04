7eee972c9a8154cc8b8033947bb2f0c1076de873
mihailgaberov <mihail.gaberov@gmail.com>
2020-02-04 08:48:07 +0200

    :sparkles: [Part 9] Adding Listings component.

--- classifieds-app/src/components/Root/Listings/Listings.js
+++ classifieds-app/src/components/Root/Listings/Listings.js
@@ -1,7 +1,52 @@
 import React from "react";
+import {useQuery} from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import styled from "styled-components";
+
+const Description = styled.p`
+  margin-bottom: 0;
+`;
+
+const Listing = styled.div`
+  padding: 1rem 0;
+  
+  :not(:last-child) {
+    border-bottom: 1px solid ${props => props.theme.veryLightGrey};
+  }
+`;
+
+const Title = styled.strong`
+  display: block;
+  font-size: 1.5rem;
+  font-weight: 700;
+`;
+
+const query = gql`
+  {
+    listings {
+      description
+      id
+      title
+    }
+  }
+`;
 
 const Listings = () => {
-  return <h1>Listings</h1>
+  const {data, loading} = useQuery(query);
+
+  console.log('>>> data', data);
+
+  if (loading) return "Loading...";
+
+  return <div>
+    {data.listings.map(listing => (
+      <Listing key={listing.id}>
+        <Title>{listing.title}</Title>
+        <Description>{listing.description}</Description>
+      </Listing>
+    ))
+    }
+  </div>
 };
 
 export default Listings;
