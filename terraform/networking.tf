a625974d6c9c319ec85f40eae25c37c2dfea015d
mihailgaberov <mihail.gaberov@gmail.com>
2020-02-09 14:17:56 +0200

    :sparkles: [Part 11] Added networking.tf

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -6,8 +6,91 @@ resource "aws_db_subnet_group" "private" {
   }
 }
 
-resource "aws_internet_gateway" "microservices-demo" {
+resource "aws_internet_gateway" "microservices" {
   vpc_id = aws_vpc.microservices.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.microservices.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.microservices.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "microservices-subnet-public" {
+  subnet_id = aws_subnet.microservices-subnet-public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "allow-internal-http" {
+  name = "allow-internal-http"
+  description = "Allow internal HTTP requests"
+  vpc_id = aws_vpc.microservices.id
+
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = [aws_vpc.microservices.cidr_block]
+  }
+}
+
+resource "aws_security_group" "allow-internal-mysql" {
+  name = "allow-internal-mysql"
+  description = "Allow internal MySQL requests"
+  vpc_id = aws_vpc.microservices.id
+
+  ingress {
+    from_port = 3306
+    protocol = "tcp"
+    to_port = 3306
+    cidr_blocks = [aws_vpc.microservices.cidr_block]
+  }
+}
+
+resource "aws_security_group" "allow-http" {
+  name = "allow-http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id = aws_vpc.microservices.id
+
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow-ssh" {
+  name = "allow-ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id = aws_vpc.microservices.id
+
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow-all-outbound" {
+  name = "allow-all-outbound"
+  description = "Allow all oubound traffic"
+  vpc_id = aws_vpc.microservices.id
+
+  ingress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_subnet" "microservices-subnet-public" {
