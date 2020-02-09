aba85216d0cb7424a13ece35a7c9b65f8c4f8108
mihailgaberov <mihail.gaberov@gmail.com>
2020-02-09 12:52:33 +0200

    :sparkles: [Part 11] Added user_data configuration for AWS linux machine

--- terraform/node-server/main.tf
+++ terraform/node-server/main.tf
@@ -7,6 +7,29 @@ resource "aws_instance" "default" {
   subnet_id = var.subnet-id
   vpc_security_group_ids = var.vpc-security-group-ids
 
+  user_data = <<EOF
+#!/bin/bash
+cd /tmp
+echo '#!/bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+source ~/.bashrc
+nvm i 12.13.0
+curl -o- -L https://yarnpkg.com/install.sh | bash
+sudo yum install -y ruby wget
+cd ~
+wget https://aws-codedeploy-eu-central-1.s3.eu-central-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo ln -s ~/.nvm/versions/node/v12.13.0/bin/node /usr/bin
+sudo ln -s ~/.nvm/versions/node/v12.13.0/bin/npm /usr/bin
+sudo ln -s ~/.yarn/bin/yarn /usr/bin
+sudo yarn global add pm2
+sudo ln -s /usr/local/bin/pm2 /usr/bin' >> init.sh
+chmod +x init.sh
+/bin/su -c "/tmp/init.sh" - ec2-user
+rm init.sh
+EOF
+
   tags = {
     Name = var.name
   }
