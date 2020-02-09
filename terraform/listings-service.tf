71ce37c0f6378ea1fdec82f1388dbf7198c6ad91
mihailgaberov <mihail.gaberov@gmail.com>
2020-02-09 15:29:36 +0200

    :sparkles: [Part 12] Added listings-service

---
+++ terraform/listings-service.tf
@@ -0,0 +1,31 @@
+resource "aws_eip" "listings-service-eip" {
+  instance = module.api-gateway.instance-id
+}
+
+module "listings-service" {
+  source = "./node-server"
+
+  ami-id = "ami-07cda0db070313c52"
+  key-pair = aws_key_pair.microservices-key.key_name
+  name = "listings-service"
+  private-ip = "10.0.1.5"
+  subnet-id = aws_subnet.microservices-subnet-private-1.id
+  vpc-security-group-ids = [
+    aws_security_group.allow-http.id,
+    aws_security_group.allow-ssh.id,
+    aws_security_group.allow-all-outbound.id
+  ]
+}
+
+module "listings-service-db" {
+  source = "./mysql-db"
+
+  apply-immediately = true
+  db-name = "db"
+  db-subnet-group-name = aws_db_subnet_group.private.id
+  identifier = "listings-service-db"
+  password = var.listings-service-db-password
+  publicly-accessible = false
+  username = var.listings-service-db-username
+  vpc-security-group-ids = [aws_security_group.allow-internal-mysql.id]
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,10 @@ variable "aws-secret-key" {
 variable "aws-region" {
     type = string
 }
+
+variable "listings-service-db-password" {
+    type = string
+}
+variable "listings-service-db-username" {
+    type = string
+}
