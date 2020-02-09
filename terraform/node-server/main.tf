c71e41f9fdf1db6b29c6e006f29c424cd2ca1c4f
mihailgaberov <mihail.gaberov@gmail.com>
2020-02-09 11:51:38 +0200

    :construction: [Part 11] Removed redundant quotes

--- terraform/node-server/main.tf
+++ terraform/node-server/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "default" {
-  ami = "var.ami-id"
-  iam_instance_profile = "var.iam-instance-profile"
-  instance_type = "var.instance-type"
-  key_name = "var.key-pair"
-  private_ip = "var.private-ip"
-  subnet_id = "var.subnet-ip"
+  ami = var.ami-id
+  iam_instance_profile = var.iam-instance-profile
+  instance_type = var.instance-type
+  key_name = var.key-pair
+  private_ip = var.private-ip
+  subnet_id = var.subnet-id
   vpc_security_group_ids = var.vpc-security-group-ids
 
   tags = {
