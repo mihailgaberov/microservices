63edc63d28e84c596953d6fcf1443f910835716a
mihailgaberov <mihail.gaberov@gmail.com>
2020-02-09 13:21:23 +0200

    :sparkles: [Part 11] Adding mysql-db. Added variables for it.

---
+++ terraform/mysql-db/variables.tf
@@ -0,0 +1,56 @@
+variable "apply-immediately" {
+  default = false
+  type = bool
+}
+
+variable "db-name" {
+  type = string
+}
+
+variable "db-subnet-group-name" {
+  type = string
+}
+
+variable "engine-version" {
+  default = "5.7"
+  type = string
+}
+
+variable "identifier" {
+  type = string
+}
+
+variable "instance-class" {
+  default = "db.12.micro"
+  type = string
+}
+
+variable "parameter-group-name" {
+  default = "default.mysql5.7"
+  type = string
+}
+
+variable "password" {
+  type = string
+}
+
+variable "publicly-accessible" {
+  type = bool
+}
+
+variable "username" {
+  type = string
+}
+
+variable "vpc-security-group-ids" {
+  default = []
+  type = list(string)
+}
+
+
+
+
+
+
+
+
