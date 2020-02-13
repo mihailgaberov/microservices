a5baa149dd0e6718fa5934a0e1b356be1b3a794b
mihailgaberov <mihail.gaberov@gmail.com>
2020-02-13 07:53:17 +0200

    :construction: [Part 14] Finishing the part - trying to deploy - Going back to use Unix based `zip` command for deploying.

--- node-deploy/src/index.js
+++ node-deploy/src/index.js
@@ -79,17 +79,15 @@ const rootDir = rel("../");
   console.log("Generating deployment file...");
   const filename = `${deploymentDirName}-deployment-${getFullDate()}.zip`;
   const zipPath = `/tmp/${filename}`;
-  // Compress-Archive -Path input.txt -DestinationPath output.zip
-  await exec(`Compress-Archive -Path ${zipPath} -DestinationPath .`);
-  /*await exec(
+  await exec(
     `zip -r ${zipPath} .
     -x terraform/\\*
     -x node_modules/\\*
     -x \\*!/node_modules/\\*
-    -x \\*!/.cache/\\*
+    -x \\*/.cache/\\*
     -x .git/\\* -x \\*.DS_Store`,
     { cwd: rootDir, maxBuffer: MAX_BUFFER_SIZE }
-  );*/
+  );
 
   console.log("Uploading deployment file...");
   await s3Client
