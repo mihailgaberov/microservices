b19044c6604514aa147bad5dce17d304a0adcf26
mihailgaberov <mihail.gaberov@gmail.com>
2020-02-18 07:45:51 +0200

    :construction: Updated api-gateway AWS instance id.

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -5,7 +5,7 @@ resource "aws_eip" "api-gateway-eip" {
 module "api-gateway" {
   source = "./node-server"
 
-  ami-id = "ami-07cda0db070313c52"
+  ami-id = "ami-0df0e7600ad0913a9"
   iam-instance-profile = module.api-gateway-codedeploy.iam-instance-profile
   key-pair = aws_key_pair.microservices-key.key_name
   name = "api-gateway"
